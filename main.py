b5f52959d54c7f083e35175236bcdda81ccd1bb6
Goa1keeper99 <jazzcrocker99@gmail.com>
2025-11-21 03:37:18 -0500

    added astroid feilds and astroids

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} \n Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    pygame.time.Clock()
+    clock = pygame.time.Clock()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
@@ -22,11 +28,12 @@ def main():
                 pygame.quit()
                 return
         screen.fill("black")
+        dt = clock.tick(60)/ 1000.0
         updatable.update(dt)
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
-        dt += pygame.time.Clock().tick(60)/ 1000.0
+        
 
 
 if __name__ == "__main__":
